1238be66cb526cfd3acf106a42ae2e4aeaa56679
Daniela DRL. Rodríguez Lainez <drodriguez@bcn.softcatalunya.com>
2021-11-05 10:13:43 +0100

    Angular componentes clientes

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,11 @@
+import { Cliente } from './cliente';
+
+
+
+export const CLIENTES: Cliente[] = [
+    {id: 1, nombre: 'Daniela', apellido: 'Rodríguez', email: 'dr@email.com', createAt: '2021-11-04'},
+    {id: 2, nombre: 'Daniela-1', apellido: 'Rodríguez-1', email: 'dr@email.com-1', createAt: '2021-11-04'},
+    {id: 3, nombre: 'Daniela-2', apellido: 'Rodríguez-2', email: 'dr@email.com-2', createAt: '2021-11-04'},
+    {id: 4, nombre: 'Daniela-3', apellido: 'Rodríguez-2', email: 'dr@email.com-3', createAt: '2021-11-04'}
+
+  ];
